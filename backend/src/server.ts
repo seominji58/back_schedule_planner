3988fe3818306845bc16ab5b47f5a2fe1120fcc1
서민지 <alswlalswl58@naver.com>
2025-07-18 14:57:57 +0900

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,7 @@
 import app from './app';
 import 'dotenv/config';
 
-const PORT = process.env['PORT'] || 3001;
+const PORT = Number(process.env['PORT']) || 3001;
 
 const startServer = async () => {
   try {
